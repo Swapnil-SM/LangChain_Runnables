7490ceb1472c0d87fccd04172b71952a3bcdef30
Swapnil-SM <swapnilmishra413@gmail.com>
2025-09-14 00:06:56 +0530

    runnable_passthrough

---
+++ Runnable_passthroough.py
@@ -0,0 +1,34 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+from langchain.schema.runnable import RunnableSequence, RunnableParallel, RunnablePassthrough
+
+load_dotenv()
+
+prompt1 = PromptTemplate(
+    template='Write a joke about {topic}',
+    input_variables=['topic']
+)
+
+model = ChatOpenAI()
+
+parser = StrOutputParser()
+
+prompt2 = PromptTemplate(
+    template='Explain the following joke - {text}',
+    input_variables=['text']
+)
+#chain to generate joke(sequence chain)
+joke_gen_chain = RunnableSequence(prompt1, model, parser)
+
+
+#parallel chain to passthrough joke and generate explanation(parallel chain)
+parallel_chain = RunnableParallel({
+    'joke': RunnablePassthrough(),
+    'explanation': RunnableSequence(prompt2, model, parser)
+})
+#merging parallel chain and sequence chain
+final_chain = RunnableSequence(joke_gen_chain, parallel_chain)
+
+print(final_chain.invoke({'topic':'cricket'}))
